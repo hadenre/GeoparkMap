f1681cc7644bb2dbdb67bdb7eb554b89e5325ab5
fooldome <luke.christison@plymouth.ac.uk>
2021-10-21 22:14:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ var dotenv = require('dotenv');
 dotenv.config();
 
 
-const { MongoClient } = require('mongodb');
+//const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://lchristison:maryjane666@geoparkdb.pfw20.mongodb.net/geoparkDB?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
+//const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//client.connect(err => {
+ // const collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  client.close();
-});
+ // client.close();
+//});
 
 
 
@@ -32,7 +32,7 @@ client.connect(err => {
 //const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 //Port Number and Connection String to DB
 var port = process.env.PORT;
-var mongoURI = client;
+var mongoURI = process.env.uri;
 
 //var port = process.env.PORT || 3000;
 //var mongoURI = process.env.mongoURI || 'mongodb://localhost:27017/geopark';
